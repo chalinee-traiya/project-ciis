6f92cc21fde17689e1a625be3ba036781daf6801
chalinee-traiya <53514040+chalinee-traiya@users.noreply.github.com>
2020-10-07 22:31:44 +0700

    text

---
+++ src/App.js
@@ -0,0 +1,101 @@
+import React from 'react';
+/*import logo from './logo.svg';*/
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
+// import ciis from './ciis.jpg';
+// import status from './status.png';
+import './App.css';
+// import List from '@material-ui/core/List'
+// import ListItem from '@material-ui/core/ListItem'
+// import ListItemText from '@material-ui/core/ListItemText'
+// import ImageResize from "image-resize";
+// import {Sidebar, InputItem, DropdownItem, Icon, Item, Logo, LogoText} from 'react-sidebar-ui'
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+
+import Login from "./components/login";
+import SignUp from "./components/signup";
+
+const App = () => {
+  return (
+    
+    // <div>
+    //   <img src={ciis} />
+    //   <Sidebar bgColor='black' isCollapsed={false}>
+        
+    //     <LogoText>Hi Admin</LogoText>
+        
+    //     <font style= {{color : 'gray'}}>Admin/Finance </font>
+    //     <ListItem >
+    //     <ListItemText>Menu</ListItemText>
+    //     </ListItem>
+ 
+    //     <Item bgColor='black'>
+    //       <Icon><i className="status"/></Icon>
+          
+    //       Check Status
+    //     </Item>
+    //     <Item bgColor='black'>
+    //       <Icon><i className="fas fa-info"/></Icon>
+    //       Check Payment
+    //     </Item>
+    //     <Item bgColor='black'>
+    //       <Icon><i className="fas fa-sitemap"/></Icon>
+    //       All research
+    //     </Item>
+        
+    //     <DropdownItem
+    //       values={['Total','Paypal','TMB']}
+    //       bgColor = {'black'}>
+    //         Paid research
+    //       </DropdownItem>
+        
+    //     <ListItem >
+    //     <ListItemText>Setting</ListItemText>
+    //     </ListItem>
+    //     <Item bgColor='black'>
+    //       <Icon><i className="fas fa-home"/></Icon>
+    //       Edit
+    //     </Item>
+    //     <Item bgColor='black'>
+    //       <Icon><i className="fas fa-info"/></Icon>
+    //       Sign out
+    //     </Item>
+        
+        
+    //   </Sidebar>
+    // </div>
+
+    <Router>
+    <div className="App">
+      <nav className="navbar navbar-expand-lg navbar-light fixed-top">
+        <div className="container">
+        <img src="/images/head.png" width="500px" /> 
+        {/* <img src="/images/head.png" width="400px" class="d-inline-block align-top" alt="" loading="lazy"></img> */}
+          <div className="collapse navbar-collapse" id="navbarTogglerDemo02">
+            <ul className="navbar-nav ml-auto">
+              <li className="nav-item">
+                <img src="/images/register.png" width="30px" />
+                <Link className="nav-link" to={"/sign-up"}>REGISTER</Link>
+              </li> 
+              <li className="nav-item">
+                <img src="/images/login.png" width="30px" />
+                <Link className="nav-link" to={"/sign-in"}>LOGIN</Link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+
+      <div className="auth-wrapper">
+        <div className="auth-inner">
+          <Switch>
+            <Route exact path='/' component={Login} />
+            <Route path="/sign-in" component={Login} />
+            <Route path="/sign-up" component={SignUp} />
+          </Switch>
+        </div>
+      </div>
+    </div></Router>
+  )
+};
+
+export default App;
---
+++ src/components/login.js
@@ -0,0 +1,37 @@
+import React, { Component } from "react";
+// import '.../images/user.png';
+export default class Login extends Component {
+    render() {
+        return (
+            <form>
+                <div class="text-center">
+                    <img src="/images/user.png" width="70px" /> 
+                </div>
+                
+                
+                <h3>LOGIN</h3>
+                
+
+                <div className="form-group">
+                    <label>Email *</label>
+                    <input type="email" className="form-control" placeholder="email" />
+                </div>
+
+                <div className="form-group">
+                    <label>Password *</label>
+                    <input type="password" className="form-control" placeholder="password" />
+                </div>
+
+                <p className="forgot-password text-right">
+                    Forgot <a href="#">password?</a>
+                </p>
+
+                <button type="submit" className="btn btn-dark rounded-pill btn-block">SIGN IN</button>
+
+                <p className="dont-have-account">
+                    Don't have account? <a href="#">Register</a>
+                </p>
+            </form>
+        );
+    }
+}
